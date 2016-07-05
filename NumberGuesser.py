78562f37c1d6e48ef049c9e3bdd8557f6822f97d
Ityer <andrewmackay1996@gmail.com>
2016-07-05 17:04:25 +0100

    Added input validation

--- NumberGuesser.py
+++ NumberGuesser.py
@@ -1,20 +1,28 @@
 import random
-Number=int(input("Number: "))
+
 
 Min=0
 Max=500000
 Done=False
 NOG=0
+while Done == False:
+	Number=input("Number: ")
+	try:
+		Number=int(Number)
+		Done=True
+	except:
+		print("Only numbers between %s and %s please."% (Min, Max))
+Done=False
 while Done==False:
 	Guess=random.randint(Min,Max)
+	NOG+=1
 	if Guess == Number:
+		print("Guess %s: %s" % (NOG, Guess))
 		print("Win")
 		Done=True
 	elif Guess > Number:
 		Max=Guess-1
-		NOG+=1
 		print("Guess %s: %s" % (NOG, Guess))
 	else:
 		Min=Guess+1
-		NOG+=1
 		print("Guess %s: %s" % (NOG, Guess))
